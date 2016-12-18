d99d8d25768a5d791e06d25b32199eea612fb6ff
Betak6630 <Betak6630@mail.ru>
2016-12-18 23:35:39 +0300

    Revert "Revert "add git""
    This reverts commit e16763423560de7f121a1e68ea84dc157348ed60.

---
+++ Skyugos/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Skyugos.Models.Contact;
+using Yugos.Utils.Helpers;
+
+namespace Skyugos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+        
+        public ActionResult Price()
+        {
+            return View();
+        }
+
+        public ActionResult Publication(int publicationId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AskQuestion(AskQuestionModel model)
+        {
+            var message = string.Format("Пишет {0}\nТема: {1}\nВопрос: {2}\nEmail: {3}\nТелефон: {4}", model.Name,
+                model.Subject, model.Message, model.Email, model.Tel);
+
+            TelegramBot.SendMessage(message);
+
+            return new JsonResult();
+        }
+    }
+}
---
+++ Skyugos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Skyugos.Startup))]
+namespace Skyugos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
