c6b7afbb15b07ec55dbd4edc43d0df434f276c5f
Betak6630 <Betak6630@mail.ru>
2016-12-18 23:03:59 +0300

    add git

---
+++ Skyugos/Models/Contact/AskQuestionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Skyugos.Models.Contact
+{
+    public class AskQuestionModel
+    {
+        public string Name { get; set; } 
+        public string Email { get; set; } 
+        public string Tel { get; set; } 
+        public string Subject { get; set; } 
+        public string Message { get; set; } 
+    }
+}
---
+++ Skyugos/wp-content/plugins/page-builder-pmc/assets/js/pmc_import.js
@@ -0,0 +1,39 @@
+﻿/* <![CDATA[ */
+
+(function ($) {
+
+
+
+    "use strict";
+
+
+
+    $(document).ready(function () {
+
+
+
+        $("#pmc-import").submit(function (e) {
+
+
+
+            if (!confirm("The import will start now!")) {
+
+                e.preventDefault();
+
+                return;
+
+            }
+
+
+
+        });
+
+
+
+    });
+
+
+
+})(jQuery);
+
+/* ]]> */
