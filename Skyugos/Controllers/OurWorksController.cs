aa64d3a62b0177b8b47dabcfc251c5370d349e8f
Betak6630 <Betak6630@mail.ru>
2017-12-10 13:57:04 +0300

    Добавил пунтк Наши работы

---
+++ Skyugos/Controllers/OurWorksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Skyugos.Models.Contact;
+using Yugos.Utils.Helpers;
+
+namespace Skyugos.Controllers
+{
+    public class OurWorksController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
